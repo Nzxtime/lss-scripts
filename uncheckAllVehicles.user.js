c061ede2cce3a4879dac97ae64f9a79a548a0834
Marcel Streicher <marcel.streicher@htl.rennweg.at>
2018-05-23 22:17:35 +0200

    Name change

--- uncheckAllVehicles.user.js
+++ uncheckAllVehicles.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
-// @name         uncheckAll
+// @name         uncheckAllVehicles
 // @version      1.0
-// @description  Entfernt alle ausgewählten Fahrzeuge mit einem Button-Klick und setzt den Zähler der AAO's vom Module AAO-Zähler des LSS-Managers 
+// @description  Entfernt alle ausgewählten Fahrzeuge mit einem Button-Klick und setzt den Zähler der AAO's vom Module AAO-Zähler des LSS-Managers
 // @author       Battlebozz
 // @include      *://www.leitstellenspiel.de/
 // @include      *://www.leitstellenspiel.de/*
@@ -13,16 +13,17 @@
 // ==/UserScript==
 
 (function () {
-    console.log('uncheckAll successfully loaded!');
+    console.log('uncheckAllVehicles successfully loaded!');
 
     function uncheckAll() {
-        $('input:checkbox').removeAttr('checked');
+        $('#vehicle_show_table_body_all input:checkbox').removeAttr('checked');
+        $('#vehicle_amount').replaceWith('<span id="vehicle_amount" class="badge vehicle_amount_selected">0</span>');
 
         if ($('.aaoZaehler').length) {
             $('.aaoZaehler').replaceWith(' <span class="aaoZaehler">0</span>');
         }
     }
 
-    $('#mission-aao-group').after('<button id="uncheckAll" class="btn  btn-default btn-xs" type="button">Uncheck All</button>');
+    $('#mission-aao-group').after('<button id="uncheckAll" class="btn  btn-default btn-xs" type="button">Uncheck All Vehicles</button>');
     $('#uncheckAll').click(uncheckAll);
 })();
